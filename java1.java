ed52a3c90d4cdf94648dda2e6909a882c2cbc8f9
Satyamsinghhhh <satyamsingh87800@gmail.com>
2026-08-06 18:45:38 +0530

    .

---
+++ java1.java
@@ -0,0 +1,7 @@
+public class java1{
+    public static void main(String[] args) {
+        int a= 15;
+        int b = 55;
+        System.out.println(a+b);
+    }
+}
---
+++ java1_2.java
@@ -0,0 +1,10 @@
+public class java1_2 {
+    public static void main(String[] args) {
+        double maths =65;
+        double english = 66;
+        double dbms = 55;
+        double total = maths + english + dbms;
+        double f = total/100*300;
+        System.out.println(f);
+    }
+}
