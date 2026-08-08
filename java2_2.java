764cb7c877a534f19b79690792ffe90f298c0010
Satyamsinghhhh <satyamsingh87800@gmail.com>
2026-08-08 18:39:35 +0530

    .

---
+++ java2_2.java
@@ -0,0 +1,7 @@
+public class java2_2{
+    public static void main(String[] args) {
+        char grade = 'a';
+        grade = (char)    (grade +8);
+        System.out.println(grade);
+    }
+}
---
+++ java2_3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class java2_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER THE NUMBER ");
+        int num = sc.nextInt();
+        System.out.println(num < 15);
+
+        sc.close();
+
+    }
+
+}
